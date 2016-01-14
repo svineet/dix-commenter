1d23b8c07d1cc5560f0e55083995029b1f56365a
Sai Vineet <saivineet89@gmail.com>
2016-01-14 21:16:41 +0530

    Make faster and fix attribute order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from xml.dom.minidom import parseString
+from lxml import etree
 import sys
 import argparse
 import re
@@ -8,31 +8,28 @@ import re
 TAG_RE = re.compile(r'<([^<>]*)>')
 
 
-def getText(nodelist):
-    rc = []
-    for node in nodelist:
-        if node.nodeType == node.TEXT_NODE:
-            rc.append(node.data)
-        if hasattr(node, 'tagName'):
-            if node.tagName == 'b':
-                rc.append(' ')
-            if node.tagName == 'g':
-                rc.append(getText(node.childNodes))
-    return ''.join(rc)
+def getText(node):
+    all_text = [node.text] if node.text else []
+    for n in node:
+        if n.tag == 'b':
+            all_text.append(' ')
+        if n.tag == 'g':
+            all_text.append(getText(n))
+        if n.tail:
+            all_text.append(n.tail)
+    return "".join(all_text)
 
 
 def comment_node(node):
-    comment = node.ownerDocument.createComment(node.toxml())
-    node.parentNode.replaceChild(comment, node)
+    comment = etree.Comment(etree.tostring(node))
+    node.getparent().replace(node, comment)
     return comment
 
 
 def search_sprop(node, attr_val):
-    for child in node.childNodes:
-        if hasattr(child, 'tagName') and child.tagName == 's':
-            if child.hasAttribute('n'):
-                if child.getAttribute('n') == attr_val:
-                    return True
+    for child in node.findall("s"):
+        if child.get("n") == attr_val:
+            return True
     return False
 
 
@@ -40,16 +37,17 @@ def go_through(dom, comment_data):
     # Constraints to be aware of:
     # 1. Text must match without tags and replace <b/>s
     # 2. <s> tags with appropriate n attributes must be present
-    # 3. groups???
+    # 3. groups
     xdddddd = comment_data.readlines()
-    comment_count = 0
-    for e in dom.getElementsByTagName('e'):
-        if not (e.getElementsByTagName('l') and e.getElementsByTagName('r')):
+    for e in dom.xpath('//e'):
+        if e.find('p') is None:
             continue
 
-        left = e.getElementsByTagName('l')[0]
-        right = e.getElementsByTagName('r')[0]
+        if e.find('p').find('l') is None or e.find('p').find('r') is None:
+            continue
 
+        left = e.find('p').find('l')
+        right = e.find('p').find('r')
         for line in xdddddd:
             constraint = {'l': [], 'r': []}
             line = line.strip()
@@ -62,41 +60,41 @@ def go_through(dom, comment_data):
 
                 for tag in TAG_RE.findall(l if key == 'l' else r):
                     constraint[key].append(tag)
+
             for c in constraint['l']:
                 if not search_sprop(left, c.strip()):
                     comment_this_line = False
 
             for c in constraint['r']:
-                if not search_sprop(right, c):
+                if not search_sprop(right, c.strip()):
                     comment_this_line = False
 
             lwords = TAG_RE.sub('', l).replace('#', '')
             rwords = TAG_RE.sub('', r).replace('#', '')
 
             word_match = False
-            if lwords == getText(left.childNodes) and \
-               rwords == getText(right.childNodes):
+            print(getText(left), getText(right), file=sys.stderr)
+            if lwords == getText(left) and \
+               rwords == getText(right):
                 word_match = True
-            if (not lwords) and rwords and rwords == getText(right.childNodes):
+            if (not lwords) and rwords and rwords == getText(right):
                 word_match = True
-            if (not rwords) and lwords and lwords == getText(left.childNodes):
+            if (not rwords) and lwords and lwords == getText(left):
                 word_match = True
             if (not lwords) and (not rwords):
                 word_match = True
 
             if comment_this_line and word_match:
-                comment_count += 1
                 comment_node(e)
                 break
-    return dom.toxml()
+    return etree.tostring(dom, encoding='utf8', method='xml')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file', help="Input file for script")
     args = parser.parse_args()
 
-    xml = sys.stdin.read()
-    dom = parseString(xml)
+    dom = etree.parse(sys.stdin)
     comment_data = open(args.input_file, 'r')
     commented_dom = go_through(dom, comment_data)
-    print(commented_dom)
+    print(commented_dom.decode('utf-8'))
